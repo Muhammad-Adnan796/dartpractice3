46b7b9c12c33f488e9476ec80dee472062b6049c
Muhammad Adnan <79781004+Adnan-Swati@users.noreply.github.com>
2021-09-22 11:04:52 -0700

    Add files via upload

---
+++ dartpractice3.dart
@@ -0,0 +1,99 @@
+void main() {
+//  int a = 12;
+//   int b = a++;
+//   print(b); 
+//    int a = 12;
+//   int b = ++a;
+//   print(b); 
+//    int a = 12;
+//   int b = --a;
+//   print(b); 
+//    int a = 12;
+//   int b = a--;
+//   print(b);
+//   int a = 2;
+//   int b = a++ + a-- - ++a + a++ - a-- + ++a;
+//           2   + 3   -  3  +  3  - 4   + 4
+//   print (b);
+//   int a = 9;
+//   int b = a + ++a - a-- + a++ + a - --a + ++a - a--;
+//           9 +  10 - 10  + 9  + 10 -  9  +  10 - 10
+//   print (b);
+//   var a = 12;
+//   var b = 12;
+//   bool result =a<b;
+//   print (result);
+//   var a = 12;
+//   var b = 12;
+//   bool result =a<=b;
+//   print (result);
+//     var a = 24;
+//   var b = 12;
+//   bool result =a>=b;
+//   print (result);
+//   int a = 12;
+//   int b = 23;
+//  bool result = a!=b;
+//   print (result);
+//   String a = 'adnan';
+//   String b = 'adnan';
+//   bool result = (a==b && a!=b);
+//   print (result);
+//    String a = 'adnan';
+//   String b = 'zeeshan';
+//   bool result = (a==b && a!=b);
+//   print (result);
+//   var  a = 25 ;
+//   var  b = 35 ;
+//   bool result = (a==b && a!=b);
+//   print (result);
+//   var  a = 25 ;
+//   var  b = 35 ;
+//   bool result = (a<=b && a!=b);
+//   print (result);
+//    var  a = 25 ;
+//   var  b = 35 ;
+//   bool result = (a<=b || a!=b);
+//   print (result);
+//     var  a = 25 ;
+//   var  b = 35 ;
+//   bool result = (a<b || a!=b);
+//   print (result);
+//   var  a = 25 ;
+//   var  b = 35 ;
+//   bool result = (a>b || a!=b);
+//   print (result);
+//    var  a = 25 ;
+//   var  b = 35 ;
+//   bool result = (a>b || a==b);
+//   print (result);
+//   int a = 10;
+//   int b = 12;
+//   bool result = !(a>b);
+//   print (result);
+//     int a = 14;
+//   int b = 12;
+//   bool result = !(a<b || a==b);
+//   print (result);
+//     int a = 14;
+//   int b = 12;
+//   bool result = !(a>b || a==b);
+//   print (result);
+//   num a = 20;
+//   num b = 20;
+//   if (a>b){
+//     print ( 'A is  equal to A');
+//   } else if (a!=b){
+//     print ('A is not equal to B');
+//   }else{
+//     print ('A is greater then or lasser then B');
+//   }
+  
+//   var lst1 = ['adnan',23,true];
+//   print (lst1);
+//   var a1 = lst1.first;
+//   print (a1);
+//   var abc = ['muhammad adnan',23,false];
+//   print (abc);
+//   print ('lst.length${abc}');
+  }
